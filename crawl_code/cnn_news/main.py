dad1714a3539365ffcd4fe1f512c0389d62357a3
psw <amollang97@naver.com>
2024-07-03 10:44:56 +0900

    create cnn code

---
+++ crawl_code/cnn_news/main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from datetime import datetime
+import pandas as pd
+import cnn_functions
+
+
+def main():
+    
+    
+    df = cnn_functions.make_df()
+    crawl_time = datetime.now().strftime("%Y-%m-%d_%H%M")
+    new_order = ['institution', 'articleTitle', 'articleContents', 'regDate', 'getDate']
+
+    # 폴더 이름
+    folder_name = 'data'
+
+    # 폴더가 없을 경우 생성
+    if not os.path.exists(folder_name):
+        os.makedirs("./"+folder_name)
+
+    df.to_csv(f"./data/cnn_{crawl_time}.csv", 
+                        index=False, sep=';', 
+                        header=True, 
+                        columns=new_order, 
+                        encoding='utf-8'
+                    )
+
+                
+
+                
+if __name__ == "__main__":
+    main()
