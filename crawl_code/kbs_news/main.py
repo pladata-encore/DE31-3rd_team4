1346fcc94dedd2b6d35d623b0598f462b662a616
JeungYeun Yoo <yjyj989812@naver.com>
2024-07-03 14:59:07 +0900

    Revert "modified kbs main file."
    This reverts commit 27c482e85465feba62d6c16bf905129a4e9de6b9.

--- crawl_code/kbs_news/main.py
+++ crawl_code/kbs_news/main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from datetime import datetime
 import pandas as pd
 
@@ -6,17 +7,26 @@ from kbs_news import kbs_functions
 from kbs_news.utils import log
 
 
-def crawl(cate_code, startDate, endDate):
-    log(f"KBS main: Request get from KBS news count. Parameters: {cate_code}, {startDate}, {endDate}", 4)
-    count_result = kbs_functions.get_kbsNews_count(cate_code=cate_code, startDate=startDate, endDate=endDate)
+def main():
+    parser = argparse.ArgumentParser(description="KBS 뉴스 크롤링")
+
+    # 명령줄 인수 정의
+    parser.add_argument('arg1', type=str, help="뉴스 카테고리 인수 ")
+    parser.add_argument('arg2', type=str, help="시작일 인수")
+    parser.add_argument('arg3', type=str, help="종료일 인수")
+    # 명령줄 인수 파싱
+    args = parser.parse_args()
+    
+    log(f"KBS main: Request get from KBS news count. Parameters: {args.arg1}, {args.arg2}, {args.arg3}", 4)
+    count_result = kbs_functions.get_kbsNews_count(cate_code=args.arg1, startDate=args.arg2, endDate=args.arg3)
     
     if count_result:
         log(f"KBS main: Request get from KBS news.", 4)
         current_time = datetime.now()
         papers = kbs_functions.get_kbsNews(news_count=count_result, 
-                                  cate_code=cate_code, 
-                                  startDate=startDate, 
-                                  endDate=endDate
+                                  cate_code=args.arg1, 
+                                  startDate=args.arg2, 
+                                  endDate=args.arg3
                                   )
         
         if papers is not False:
@@ -38,6 +48,10 @@ def crawl(cate_code, startDate, endDate):
                     )
             log(f"KBS main: Sucess create new file './data/kbs_{crawl_time}.csv'", 4)
         else:
-            log(f"KBS main: Crawl passed. Teh number of papers is 0. Parameters: {cate_code}, {startDate}, {endDate}", 4)
+            log(f"KBS main: Crawl passed. Teh number of papers is 0. Parameters: {args.arg1}, {args.arg2}, {args.arg3}", 4)
     else:
-        log(f"KBS main: KBS request Failed. The status code is not 200. Parameters: {cate_code}, {startDate}, {endDate}", 1)
+        log(f"KBS main: KBS request Failed. The status code is not 200. Parameters: {args.arg1}, {args.arg2}, {args.arg3}", 1)
+
+
+if __name__ == "__main__":
+    main()
