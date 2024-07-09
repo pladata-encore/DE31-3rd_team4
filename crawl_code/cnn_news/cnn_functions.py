362e29fad8b1ec004663d298b4d71e90e01da587
psw <amollang97@naver.com>
2024-07-09 17:30:33 +0900

    edit cnn crawler

--- crawl_code/cnn_news/cnn_functions.py
+++ crawl_code/cnn_news/cnn_functions.py
@@ -3,41 +3,76 @@ import requests
 from bs4 import BeautifulSoup as BS
 import pandas as pd
 from datetime import datetime
-from glob import glob
+import re
+
+def get_required_id():
+    # 첫 번째 웹 페이지 URL을 지정합니다.
+    url1 = 'https://edition.cnn.com/search?q=&from=0&size=10&page=1&sort=newest&types=all&section='
+    
+    response1 = requests.get(url1)
+    soup = BS(response1.text, 'html.parser')
+    
+    # <script type="text/javascript"> 태그 찾기
+    scripts = soup.find_all('script', {'type': 'text/javascript'})
+    
+    # uuidv4 값을 찾기
+    uuidv4_value = None
+    for script in scripts:
+        script_str = str(script.string)
+        pattern = r'uuidv4\(\)'
+        match = re.search(pattern, script_str)
+        if match:
+            uuidv4_value = match.group()
+
+    return uuidv4_value
 
 
 def make_df():
-    """
-    It will give you dataframe made from cnn news webpage
-    """
+
+    # id 토큰 받아오기
+    uuidv4_value = get_required_id()
     
-    url = "https://edition.cnn.com/business"
-    r = requests.get(url)
-    bs = BS(r.text)
+    # 두 번째 웹 페이지 URL을 지정
+    size = 100
+    from_ =  0
+    page = 1
+    sort = "newest"
+    url2 = f'https://search.prod.di.api.cnn.io/content?q=&size={size}&from={from_}&page={page}&sort={sort}&request_id={uuidv4_value}'
     
-    # 기사 추가링크 생성
-    alist = bs.find_all('a', class_="container__link container__link--type-article container_lead-plus-headlines__link")
+    # requests 세션을 생성
+    session = requests.Session()
     
+    # 세션을 유지하며 두 번째 웹 페이지를 가져옴
+    response2 = session.get(url2)
+    articlelink = response2.json()['result']
+    
+    # 기사 링크 리스트 생성
+    newslink = []
+    titlelist = []
+    for x in articlelink:
+        pathlink = x['path']
+        if pathlink.split('/')[-1] == 'index.html' and pathlink.split('/')[-3] != 'live-news':
+            newslink.append(pathlink)
+            titlelist.append(x['headline'])
+        
     # category
     category = []
-    for a in alist:
-        category.append(a.get('href').split('/')[-3])
+    for a in newslink:
+        category.append(a.split('/')[-3])
         
-    # 새로운 링크로 크롤링
-    new_bs = []
-    for x in alist:
-        r = requests.get(url[:-9] + x.get('href'))
-        new_bs.append(BS(r.text))
+    
+    # 각 링크로 크롤링
+    newbs= []
+    for link in newslink:
+        newurl = link
+        r = requests.get(link)
+        newbs.append(BS(r.text))
         
         
     # 데이터프레임 준비작업
-    titlelist=[]
     articlelist=[]
     regDate=[]
-    for x in new_bs:
-        # title
-        titlelist.append(x.find('div', class_="headline__wrapper").text.strip())
-    
+    for x in newbs:    
         # contents
         contentlist = x.find('div', class_="article__content").find_all('p', class_="paragraph inline-placeholder vossi-paragraph-primary-core-light")
         for i, a in enumerate(contentlist):
